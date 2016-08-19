c092c77ae7c3cb4b4791c0d363ab8c1997753f93
jamestothe <jamesdelacruz@gmail.com>
2016-08-19 13:58:55 -0700

    added cities index and cities controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818233929) do
+ActiveRecord::Schema.define(version: 20160819205330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "picture"
+    t.string   "city_website"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "city_users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "city_users", ["city_id"], name: "index_city_users_on_city_id", using: :btree
+  add_index "city_users", ["user_id"], name: "index_city_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -25,4 +44,6 @@ ActiveRecord::Schema.define(version: 20160818233929) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "city_users", "cities"
+  add_foreign_key "city_users", "users"
 end
