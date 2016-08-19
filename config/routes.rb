e6055cea8e1be5a686aec6c3bc92932ed7a42743
sophn11 <sophianakpil@gmail.com>
2016-08-18 20:18:49 -0700

    recreated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
     root to: "welcome#index"
 
+    # new user routes
     get "/users", to: "users#index", as: "users"
     get "/signup", to: "users#new", as: "new_user"
-    post "users", to: "users#create"
+    post "/users", to: "users#create"
+    get "/users/:id", to: "users#show", as: "user"
+
+    #sessions routes
+    get "/login", to: "sessions#new", as: "login"
+    get "/logout", to: "sessions#destroy", as: "logout"
+    post "/sessions", to: "sessions#create"
 end
