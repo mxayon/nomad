2313f872cd131048a1a451f4ae83049fa0475dc6
jamestothe <jamesdelacruz@gmail.com>
2016-08-19 14:13:27 -0700

    fully cruded cities... plus join table -still needs to be CRUDED-

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,37 @@
+class CitiesController < ApplicationController
+  def index
+    @cities = City.all
+    render :index
+  end
+  def new
+    @city = City.new
+    render :new
+  end
+  def create
+    city_params = params.require(:city).permit(:name, :description, :city_website, :picture)
+    @city = City.create(city_params)
+    redirect_to cities_path
+  end
+  def show
+    @city = City.find_by_id(params[:id])
+    render :show
+  end
+  def edit
+    city_id = params[:id]
+    @city = City.find_by_id(city_id)
+    render :edit
+  end
+  def update
+    city_id = params[:id]
+    @city = City.find_by_id(city_id)
+    city_params = params.require(:city).permit(:name,:description, :city_website, :picture)
+    @city.update_attributes(city_params)
+    redirect_to city_path(@city)
+  end
+  def destroy
+    city_id = params[:id]
+    @city = City.find_by_id(city_id)
+    @city.destroy
+    redirect_to cities_path
+  end
+end
