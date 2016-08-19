37eb27821d48e6b940b4d23287160d74b838394f
WDIDavidKim <davidjkim1984@gmail.com>
2016-08-18 18:19:01 -0700

    added post route for user#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     root to: "welcome#index"
 
     get "/users", to: "users#index", as: "users"
-    get "/users/new", to: "users#new", as: "new_user"
+    get "/signup", to: "users#new", as: "new_user"
+    post "users", to: "users#create"
 end
