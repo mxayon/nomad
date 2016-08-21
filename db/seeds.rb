dd71c4c20c51d73a6e53c32bb4a42cb0bf809b48
sophn11 <sophianakpil@gmail.com>
2016-08-20 21:00:34 -0700

    added profile picture and fixed show

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,25 +3,29 @@ USERS = [
   first_name: "David",
   last_name: "Kim",
   email: "davidkim@switt.com",
-  password_digest:  "$2a$10$IhBFOpRHZSsP/BFLWC.Ic.9CHwvh/tyVne7DrcPTKxH6OUqGfRcM2"
+  password_digest:  "$2a$10$IhBFOpRHZSsP/BFLWC.Ic.9CHwvh/tyVne7DrcPTKxH6OUqGfRcM2",
+  photo: "http://i.imgur.com/49Iw8sS.jpg"
 },
 {
   first_name: "Sophia",
   last_name: "Nakpil",
   email: "sophianakpil@switt.com",
-  password_digest:  "$2a$10$WnUKsCQcEAfqhHyfqLVovOaYt2glbp4xw0eLbjvixRxUxyYXKbvB."
+  password_digest:  "$2a$10$WnUKsCQcEAfqhHyfqLVovOaYt2glbp4xw0eLbjvixRxUxyYXKbvB.",
+  photo: "http://i.imgur.com/49Iw8sS.jpg"
 },
 {
   first_name: "Paolo",
   last_name: "Sese",
   email: "paolosese@switt.com",
-  password_digest:  "$2a$10$u4ZLO23FqTKffhxJN6D25..X1EEDigUMAy/J23CGl1MaAwqEXB38y"
+  password_digest:  "$2a$10$u4ZLO23FqTKffhxJN6D25..X1EEDigUMAy/J23CGl1MaAwqEXB38y",
+  photo: "http://i.imgur.com/49Iw8sS.jpg"
 },
 {
   first_name: "James",
   last_name: "de la Cruz",
   email: "jamesdelacruz@switt.com",
-  password_digest:  "$2a$10$4IP3UySsfUR36oAZ1/UT/OVae7swOJkIfyHEIeFMxRvywizOP.yLm"
+  password_digest:  "$2a$10$4IP3UySsfUR36oAZ1/UT/OVae7swOJkIfyHEIeFMxRvywizOP.yLm",
+  photo: "http://i.imgur.com/49Iw8sS.jpg"
 }
 ]
 
@@ -58,21 +62,21 @@ POSTS = [
   city_id: 1,
   title: "Marin Headlands, Graffiti bunkers",
   content: "Described as the Holy Grail for local urban explorers",
-  picture: "http://photos2.meetupstatic.com/photos/event/e/5/f/8/highres_265978872.jpeg"
+  picture: "http://images.fineartamerica.com/images-medium-large-5/graffiti-by-the-golden-gate-bridge-sarit-sotangkur.jpg"
   },
   {
   user_id: 2,
   city_id: 2,
   title: "Lava House",
   content: "Built by Jerry Ganz (the fabulously wealthy man responsible for the mass production of seat-belts) in the 1970s, the Lava House's chief architectural pull was its extensive use of imported lava rock. Construction stopped suddenly shortly after construction began, and it has remained empty to this day.",
-  picture: "http://4.bp.blogspot.com/_5jbMo5gkQWc/S710Pz3dwNI/AAAAAAAAAik/E-EJwTlde28/s1600/DSC_0694.JPG"
+  picture: "http://4.bp.blogspot.com/_5jbMo5gkQWc/S710Pz3dwNI/AAAAAAAAAik/E-EJwTlde28/s1600/DSC_0694.jpg"
   },
   {
   user_id: 3,
   city_id: 3,
   title: "Gray Whale Cove, Pacifica",
   content: "The beach (a.k.a. Devil's Slide) features a sheltered cove surrounded by cliffs that drop abruptly into the Pacific Ocean. A steep trail leads down to the beach. There is a small picnic area on the bluff above.  Gray whales can often be seen close to the shore.",
-  picture: "http://www.outdoorproject.com/sites/default/files/styles/cboxshow/public/1403213456/img_2703.jpg?itok=HYYt2HUp"
+  picture: "http://www.outdoorproject.com/sites/default/files/styles/cboxshow/public/1403213456/img_2703.jpg"
   },
   {
   user_id: 4,
